336c479ecf4275ebd52c2d8dd5307549e7ce4aa3
cwest9400 <chrstphrwest@gmail.com>
2023-01-03 15:39:36 -0500

    first setup

---
+++ server.js
@@ -0,0 +1,30 @@
+// import express
+const express = require("express");
+// create application object
+const app = express();
+
+const cors = require('cors')
+const morgan = require('morgan')
+
+const movieController = require('./controllers/product-controller')
+const reviewController = require('./controllers/review-controller')
+
+
+// initialize .env variables
+require("dotenv").config();
+require("./config/db.connection")
+// pull PORT from .env, give default value of 4000 and establish DB Connection
+
+const PORT = process.env.PORT || 4000
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+app.use('/movie', movieController)
+app.use('/review', reviewController)
+app.get('/', (req, res)=>res.redirect('/movie'))
+
+app.listen(PORT, ()=> {
+    console.log(`listening on: ${PORT}`)
+})
