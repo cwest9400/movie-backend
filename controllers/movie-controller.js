96411ab1625c16000f19d0aa00c3a42b3d7bf1f6
cwest9400 <chrstphrwest@gmail.com>
2023-01-03 16:01:47 -0500

    movie-controller -update, create, delete finished

--- controllers/movie-controller.js
+++ controllers/movie-controller.js
@@ -29,3 +29,41 @@ router.get('/:id', async (req,res)=> {
         res.status(400).json({error: err})
     }
 })
+
+//create movie route: http://localhost:4000/movie
+router.post('/', async (req, res, next)=> {
+    try {
+        const createdMovie = await Movie.create(req.body)
+        console.log(createdMovie)
+        res.status(201).json(createdMovie)
+    } catch(error) {
+        console.error(error)
+        return next(error)
+    }
+})
+
+//update Movie route: http://localhost:4000/movie/:id
+router.put('/:id', async (req, res, next)=>{
+    try {
+        const updatedMovie = await Movie.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        console.log(updatedMovie)
+        return res.status(200).json(updatedMovie)
+    } catch(error) {
+        console.error(error)
+        return next(error)
+    }
+})
+
+//delete product: http://localhost:4000/movie/:id
+router.delete('/:id', async (req,res,next)=> {
+    try {
+        const deletedMovie = await Movie.findByIdAndDelete(req.params.id)
+        console.log(deletedMovie)
+        res.redirect('/products')
+    } catch(error) {
+        console.error(error)
+        return next(error)
+    }
+})
+
+module.exports = router
