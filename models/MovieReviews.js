8d2f9b77b2332405795b033f0261ca030c0d9872
cwest9400 <chrstphrwest@gmail.com>
2023-01-03 20:20:16 -0500

    added movieReviews schema

---
+++ models/MovieReviews.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+const Movie = require("./Movie")
+
+const MovieReviewSchema = new mongoose.Schema({
+   
+    rating: {
+        type: Number,
+        required: [true],
+        minlegnth: 1,
+        maxlength: 100
+    },
+    comment: {
+        type: String,
+        required: true,
+        max: 250
+    },
+    //relationship to movie
+    title: {
+        type: mongoose.Types.ObjectId,
+        ref: "Movie",
+    },
+}, {timestamps: true})
+
+const ProductReview = mongoose.model("Review", ProductReviewSchema)
+module.exports = ProductReview
